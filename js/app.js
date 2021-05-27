69def309eb8b1f289190f632a76b745ad884350d
Amarja Palekae <amarjapalekar09@gmail.com>
2021-05-27 12:09:49 -0700

    Feart: Add JS file to the index html

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+
+console.log("File is attached");
